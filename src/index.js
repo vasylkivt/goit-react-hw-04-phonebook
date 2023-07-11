b43c88ed13f027e74d578b02d24296bf47a02cfc
Taras <117749635+vasylkivt@users.noreply.github.com>
2023-07-11 21:23:26 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ import { ThemeProvider } from 'styled-components';
 import { theme, GlobalStyle } from 'styles';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-  <ThemeProvider theme={theme}>
-    <GlobalStyle />
-    <App />
-  </ThemeProvider>
-  //</React.StrictMode>
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
 );
